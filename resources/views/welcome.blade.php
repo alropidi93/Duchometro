ee9e8306e088d67a4c306174a14ab33508bfd70e
Alvaro Picho <alvaro.picho@pucp.pe>
2017-08-01 23:28:02 -0500

    Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
 				<h1>
 					<img src="img/shower.png">
 					<span id="temperature"><i class="fa fa-tint" aria-hidden="true"></i>078<span>Lts</span></span>
-					Duchómetro Limeño<br>
+					Duchómetro limeño<br>
 					<small>¿A cuánto equivale tu ducha?</small>
 				</h1>
 			</div>
